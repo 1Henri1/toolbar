81276d0be7f12fc4e14e86d915b9bd22904cf279
1henri1 <henri135246@gmail.com>
2023-03-30 21:40:56 -0300

    menu

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -3,12 +3,28 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+    // DECLARANDO VARIAVEL GLOBAL QUE IRA REPRESENTAR O XML NO JAVA
+    MaterialToolbar idToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // APRESENTANDO O XML PARA O JAVA
+        idToolBar = findViewById(R.id.idToolBar);
+
+        idToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "cliquei no menu hamburguer", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
